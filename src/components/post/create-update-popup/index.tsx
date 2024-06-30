5982b84565e8f42f3da8179786327fffd76e9c4e
AchrafGroiez <41470d0a@gmail.com>
2024-06-30 16:37:27 -0400

    Changed Modal to Dialog

--- src/components/post/create-update-popup/index.tsx
+++ src/components/post/create-update-popup/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Modal } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Modal, Stack } from "@mui/material";
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import * as yup from 'yup';
+import { array, object, string } from 'yup';
 import { PostDTO } from "../../../model/dto/PostDTO";
 import { Category } from "../../../model/enums/Category";
 import { createPost, updatePost } from "../../../services/PostService";
@@ -24,12 +24,15 @@ export interface CreatePopupProps {
 export default function CreatePopup(props: CreatePopupProps) {
     return (
         <>
-            <Modal
+            <Dialog
                 open={props.open}
                 onClose={props.reset}
-                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                maxWidth={false}
+                PaperProps={{ sx: { borderRadius: "10px", padding : "13px" } }}
             >
-                <Box sx={{ background: "white", width: 800, padding: 3, borderRadius: "10px" }}>
+                <DialogTitle variant="h5" sx={{fontWeight : "bold"}}>{props.isUpdate ? "Update" : "Create"} post</DialogTitle>
+
+                <DialogContent>
                     <Formik
                         initialValues={{
                             id: props.postViewDto?.id,
@@ -39,22 +42,19 @@ export default function CreatePopup(props: CreatePopupProps) {
                             category: props.postViewDto?.category ?? Category.OTHER
                         }}
                         validationSchema={
-                            yup.object(
+                            object(
                                 {
-                                    title: yup
-                                        .string()
+                                    title: string()
                                         .required("Title is required.")
                                         .max(80, "Title cannot be more than 80 characters.")
                                         .min(10, "Title cannot be less than 10 characters.")
                                     ,
-                                    description: yup
-                                        .string()
+                                    description: string()
                                         .required("Description is required")
                                         .max(5000, "Description cannot be more than 80 characters.")
                                         .min(10, "Description cannot be less than 10 characters.")
                                     ,
-                                    tags: yup
-                                        .array()
+                                    tags: array()
                                         .min(3, TagsErrors.SIZE)
                                 }
                             )
@@ -78,54 +78,51 @@ export default function CreatePopup(props: CreatePopupProps) {
                         }}
                     >
                         {({ values, setFieldValue }) => (
-                            <Form>
-                                <h2>{props.isUpdate ? "Update" : "Create"} post</h2>
+                            <Form id="create-update-post">
+                                <Stack spacing={4}>
 
-                                <Field
-                                    name="title"
-                                    label="Title"
-                                    component={CreateInput}
-                                />
-                                <br /><br /><br />
+                                    <Field
+                                        name="title"
+                                        label="Title"
+                                        component={CreateInput}
+                                    />
 
-                                <Field
-                                    name="description"
-                                    label="Description"
-                                    component={CreateInput}
-                                    isTextArea
-                                />
-                                <br /><br /><br />
+                                    <Field
+                                        name="description"
+                                        label="Description"
+                                        component={CreateInput}
+                                        isTextArea
+                                    />
 
-                                {/* Hide Category if it is update */}
-                                {props.isUpdate ?
-                                    (<></>) : (
-                                        <>
+                                    {/* Hide Category if it is update */}
+                                    {props.isUpdate ?
+                                        (<></>) : (
                                             <Field
                                                 name="category"
                                                 component={CategorySelector}
                                             />
-                                            <br /><br /><br />
-                                        </>
-                                    )
-                                }
+                                        )
+                                    }
 
-                                <Field
-                                    name="tags"
-                                    component={Tags}
-                                    error={<ErrorMessage name="tags" />}
-                                    tags={values.tags}
-                                    setTags={(tags: string[]) => setFieldValue("tags", tags)}
-                                />
+                                    <Field
+                                        name="tags"
+                                        component={Tags}
+                                        error={<ErrorMessage name="tags" />}
+                                        tags={values.tags}
+                                        setTags={(tags: string[]) => setFieldValue("tags", tags)}
+                                    />
+                                </Stack>
 
-                                <Box sx={{ display: 'flex', justifyContent: 'space-between', width: "180px", float: "right" }}>
-                                    <Button variant="text" onClick={props.reset}>Cancel</Button>
-                                    <Button type="submit" variant="contained">{props.isUpdate ? "Update" : "Create"}</Button>
-                                </Box>
                             </Form>
                         )}
                     </Formik>
-                </Box>
-            </Modal>
+                </DialogContent>
+
+                <DialogActions>
+                    <Button variant="text" onClick={props.reset}>Cancel</Button>
+                    <Button type="submit" variant="contained" form="create-update-post">{props.isUpdate ? "Update" : "Create"}</Button>
+                </DialogActions>
+            </Dialog >
         </>
     );
 }
