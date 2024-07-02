c373cd25d383efdde20bb345660de77b5a9d76a3
AchrafGroiez <41470d0a@gmail.com>
2024-07-02 18:33:13 -0400

    Added a navbar in the layout.tsx and added a home page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Button, Stack, TextField, Typography, Link as MuiLink } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,24 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<nav style={{ padding: "15px" }}>
+					<Stack direction="row" justifyContent="space-between">
+						<Stack spacing={2} direction="row">
+							<MuiLink href="/" component={Link} underline="hover">Home</MuiLink>
+							<MuiLink href="/trend" component={Link} underline="hover">Trend</MuiLink>
+							<MuiLink href="/comments" component={Link} underline="hover">Community</MuiLink>
+						</Stack>
+
+						<Stack spacing={2} direction="row">
+							<Button variant="outlined">Sign up</Button>
+							<Button variant="contained">Get Started</Button>
+						</Stack>
+					</Stack>
+				</nav>
+
+				{children}
+			</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+
 export default function Home() {
 	return (
-		<div>
-			Hello World!
-		</div>			
+		<Stack direction="row" alignItems="center" justifyContent="center" height={"80vh"} spacing={1.5}>
+			<Stack spacing={3} width={500}>
+				<Typography variant="h1" fontFamily="sans-serif">Everyone as a voice</Typography>
+				<Typography variant="h6" fontFamily="monospace">The place where all your questions can be answered.</Typography>
+				<TextField placeholder="Search something" />
+			</Stack>
+
+			<Image src="/old-computers.webp" alt="background" width={612} height={408} />
+		</Stack>
 	);
 }
