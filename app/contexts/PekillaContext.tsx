3af21f1f1412097d99f4f5ad99e98e5bcc0ead9b
AchrafGroiez <41470d0a@gmail.com>
2024-07-05 13:13:19 -0400

    Disactivated noSsr and defaultMatches to hide error.

--- app/contexts/PekillaContext.tsx
+++ app/contexts/PekillaContext.tsx
@@ -9,14 +9,18 @@ export const PekillaContext = createContext({
     userId: config.id
 });
 
+/**
+ const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', { 
+    // Give the defaultValues.
+    defaultMatches : window.matchMedia('(prefers-color-scheme: dark)').matches,
+    // To do not rerender after the first mount
+    noSsr : true
+});
+ */
+
 export function PekillaContextProvider(props: { children: any }) {
     // It is a listener, when the browser change, the site change.
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', { 
-        // Give the defaultValues.
-        defaultMatches : window.matchMedia('(prefers-color-scheme: dark)').matches,
-        // To do not rerender after the first mount
-        noSsr : true
-    });
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
     const theme = createTheme({
         palette: {
