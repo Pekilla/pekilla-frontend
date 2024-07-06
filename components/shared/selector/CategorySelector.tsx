24251bed118c9a2ed897e80aafadc9cfe9656570
AchrafGroiez <41470d0a@gmail.com>
2024-07-06 00:28:28 -0400

    Putted back the category icon.

--- components/shared/selector/CategorySelector.tsx
+++ components/shared/selector/CategorySelector.tsx
@@ -3,10 +3,11 @@ import { CATEGORIES } from "@/models/enums/Category";
 import { createRandomKey } from "@/utils/RandomKeys";
 import { MenuItem } from "@mui/material";
 import { FieldProps } from "formik";
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 export default function CategorySelector(props: FieldProps) {
     return (
-        <Selector label="Category" {...props}>
+        <Selector label="Category" {...props} icon={<FormatListBulletedIcon />}>
             {CATEGORIES.map(
                 category => (
                     <MenuItem key={createRandomKey()} value={category}>{category.toLowerCase()}</MenuItem>
